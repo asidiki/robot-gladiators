493d213b240c9eaec87c2d780c1bbaa5aad4f3c1
Ansab Sidiki <ansab@sidiki.io>
2022-02-17 18:23:17 -0500

    added for loop for fight function and enemyNames array)
    :wq

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,18 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 var playerMoney = 10;
 
-var fight = function() {
+
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -43,6 +45,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   } else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+
   // if player choses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
   // confirm player wants to skip
@@ -58,10 +61,13 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   else {
     fight();
   }
+
 } else {
   window.alert("You need to choose a valid option. Try again!");
 }
 
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
